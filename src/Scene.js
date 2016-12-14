99ad5856c0646ffd6f36c50f417a2baf9280b1a5
CodeMeow <codemeow@yahoo.com>
2016-12-14 14:49:52 +0800

    Change element display mode from block to inline-block

--- src/Scene.js
+++ src/Scene.js
@@ -79,6 +79,7 @@ export default class Scene{
         this.elements[box.handle] = box;
         box.element.attr('handle', box.handle);
         box.element.css('pointer-events', 'auto');
+        box.element.css('display', 'inline-block');
         box.element.css('position', 'relative');
         box.element.css('left', box.x);
         box.element.css('top', box.y);
